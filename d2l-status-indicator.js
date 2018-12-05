8aab3dd3fdfb06da9295641b67d50d63cda34f7d
Dave Lockhart <dave.lockhart@d2l.com>
2018-12-05 10:38:56 -0500

    Polymer 3 Conversion

--- d2l-status-indicator.js
+++ d2l-status-indicator.js
@@ -3,11 +3,15 @@
 Polymer-based web component for a D2L status indicator
 @demo demo/index.html
 */
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+import '@polymer/polymer/polymer-legacy.js';
 
-import '../@polymer/polymer/polymer-legacy.js';
-
-import '../d2l-colors/d2l-colors.js';
-import { Polymer } from '../@polymer/polymer/lib/legacy/polymer-fn.js';
+import 'd2l-colors/d2l-colors.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
 const $_documentContainer = document.createElement('template');
 
 $_documentContainer.innerHTML = `<dom-module id="d2l-status-indicator">
@@ -51,7 +55,7 @@ $_documentContainer.innerHTML = `<dom-module id="d2l-status-indicator">
 		</style>
 		[[text]]
 	</template>
-
+	
 </dom-module>`;
 
 document.head.appendChild($_documentContainer.content);
